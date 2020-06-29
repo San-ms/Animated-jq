ceaa6d684a6faf9c86d4edbed827b14a1ad45813
San-ms <63304825+San-ms@users.noreply.github.com>
2020-06-29 18:30:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// alert ("Hello")
+
+$("nav a").on("click", function (event) {
+// detener que pase algo por default
+event.preventDefault()
+
+var href = $(this).attr("href")
+
+window.history.pushState(null, href)
+
+$("nav a").removeClass("active")
+
+$(this).addClass("active")
+
+$.ajax({
+  url:href,
+  success: function (data) {
+    $("section").fadeOut(250)
+  }
+})
+
+
+
+})
